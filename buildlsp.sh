a9d86f9937a8105e69ccb24e986a5d56ee4bba01
AryanRogye <161079183+AryanRogye@users.noreply.github.com>
2025-12-09 15:23:50 -0600

    added tests

--- buildlsp.sh
+++ buildlsp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-xcode-build-server config -project ComfyEditor.xcodeproj -scheme ComfyEditor
+# xcode-build-server config -project ComfyEditor.xcodeproj -scheme ComfyEditor
+
+xcode-build-server config -project ComfyEditor.xcodeproj -scheme ComfyEditor_JUST_EDITOR
